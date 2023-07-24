8b1f20536f52bc14aff06374dd5cade397fe7b5e
Jonathon <jonathon.surles@k12.lcisd.net>
2023-07-24 00:38:51 -0500

    1-ply material-based evaluation implemented

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,36 @@
 ﻿using ChessChallenge.API;
+using static ChessChallenge.Application.ConsoleHelper;
 
 public class MyBot : IChessBot
 {
+
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move bestMove = Move.NullMove;
+        int bestEval = -100000;
+        int mult = board.IsWhiteToMove ? 1 : -1;
+        foreach (Move move in moves) {
+            int eval = mult * Eval(board, move);
+            if (eval > bestEval) {
+                bestMove = move;
+                bestEval = eval;
+            }
+        }
+        return bestMove;
     }
+
+    private int Eval(Board board, Move move)
+    {  
+        int[] weights = {100, 300, 300, 500, 900, 30000, -100, -300, -300, -500, -900, -30000};
+        int eval = 0;
+        board.MakeMove(move);
+        PieceList[] lists = board.GetAllPieceLists();
+        for (int i = 0; i < lists.Length; i++) {
+            eval += weights[i] * lists[i].Count;
+        }
+        board.UndoMove(move);
+        return eval;
+    }
+
 }
