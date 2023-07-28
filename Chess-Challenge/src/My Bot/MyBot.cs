88a4ced128983daccb781b19c901c24d49d2a3b8
Jonathon <jonathon.surles@k12.lcisd.net>
2023-07-27 23:07:02 -0500

    Positional Evaluation Version 1

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -54,7 +54,7 @@ public class MyBot : IChessBot
             }
         }
 
-        // For checkmates, we represent o 999999 is +M1, 999998 is +M2, -999999 is -M1, etc.
+        // For checkmates, we represent mate in N ply as 1,000,000 - N (and -1,000,000 + N for black)
         if (bestEval > 900_000) {
             bestEval--;
         } else if (bestEval < -900_000) {
@@ -65,7 +65,6 @@ public class MyBot : IChessBot
     }
 
     // Heuristic evaluation function
-    // TODO: positional evaluation
     private int Eval(Board board)
     {  
         // Check for end of game
@@ -80,16 +79,68 @@ public class MyBot : IChessBot
             return 0;
         }
 
-        int[] weights = {0, 100, 300, 300, 500, 900, 0};
-        int materialEval = 0;
+        int evaluation = 0;
         PieceList[] lists = board.GetAllPieceLists();
         foreach (PieceList list in lists) {
-            // Material evaluation
-            materialEval += weights[(int)list.TypeOfPieceInList] * list.Count * (list.IsWhitePieceList ? 1 : -1);
-            // Positional evaluation
+            // Material and positional evaluation
+            for (int i = 0; i < list.Count; i++) {
+                Piece piece = list.GetPiece(i);
+                int file = piece.Square.File < 4 ? piece.Square.File : 7 - piece.Square.File;
+                int rank = piece.IsWhite ? piece.Square.Rank : 7 - piece.Square.Rank;
+                int pieceEval = 0;
+                switch ((int)piece.PieceType) {
+                    // TODO Pawns
+                    case 1:
+                        int[] PFileEval = {9, 9, 10, 13};
+                        int[] PRankEval = {-10, -9, -13, -5, 1, 7, 40, -10};
+                        pieceEval += 100 + PFileEval[file] + PRankEval[rank];
+                        break;
+
+                    // Knights
+                    case 2:
+                        int[] NFileEval = {-38, -14, -1, 2};
+                        int[] NRankEval = {-25, -2, 11, 15, 14, 13, -1, -25};
+                        pieceEval += 320 + NFileEval[file] + NRankEval[rank];
+                        break;
+
+                    // Bishops
+                    case 3:
+                        int[] BFileEval = {-13, -0, 1, 3};
+                        int[] BRankEval = {-10, 1, 7, 5, 5, 3, 0, -10};
+                        pieceEval += 330 + BFileEval[file] + BRankEval[rank];
+                        break;
+
+                    // Rooks
+                    case 4:
+                        // // Material Evaluation
+                        // pieceEval += 500;
+                        // // Bonus for 7th rank
+                        // if (rank == 6) pieceEval += 10;
+                        // // Penalty for edge file
+                        // if (file == 1 && (rank != 0 || rank != 7)) pieceEval += -5;
+                        // // Bonus for centralized on 1st rank
+                        // if (rank == 0 && file == 3) pieceEval += 5;
+                        int[] RFileEval = {-3, 1, 1, 1};
+                        int[] RRankEval = {0, -2, -2, -2, -2, -2, 8, 0};
+                        pieceEval += 500 + RFileEval[file] + RRankEval[rank];
+                        break;
+
+                    // Queens
+                    case 5:
+                        int[] QFileEval = {-11, -2, 1, 1};
+                        int[] QRankEval = {-9, 1, 4, 5, 4, 3, 0, -9};
+                        pieceEval += 900 + QFileEval[file] + QRankEval[rank];
+                        break;
+
+                    // TODO Kings
+                    case 6:
+                        break;
+                }
+                evaluation += pieceEval * (piece.IsWhite ? 1 : -1);
+            }
         }
 
-        return materialEval;
+        return evaluation;
     }
 
     // // Yoinked from https://stackoverflow.com/a/12171691/21553030
